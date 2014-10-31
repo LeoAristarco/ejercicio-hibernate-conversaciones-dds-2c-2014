756a2fa665af8cfccfe49e991760067ed7e6ff52
Santiago <saantii.28@hotmail.com>
2014-10-31 18:14:35 -0300

    Le agrego un orden a los mensajes de las conversaciones

--- src/main/java/model/Conversacion.java
+++ src/main/java/model/Conversacion.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 import javax.persistence.*;
 
+import org.hibernate.annotations.IndexColumn;
+
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "conv_tipo", discriminatorType = DiscriminatorType.STRING)
 public abstract class Conversacion extends EntidadPersistente {
 
 	@OneToMany
+	@IndexColumn(name="orden")
 	@JoinColumn(name="conversacion_id")
 	private List<Mensaje> mensajes = new ArrayList<Mensaje>();
 
